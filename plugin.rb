53dd1e76426410498046e006c1ba849543643cb5
Dan <Dan@CrosseyeJack.com>
2016-10-17 19:25:27 +0100

    Deving

--- plugin.rb
+++ plugin.rb
@@ -70,8 +70,7 @@ auth_provider title: 'with Twitch',
     message: 'Log in with Twitch (Make sure pop up blockers are not enabled).',
     frame_width: 960,
     frame_height: 800,
-    authenticator: TwitchAuthenticator.new('twitch', trusted: true,
-      auto_create_account: true)
+    authenticator: TwitchAuthenticator.new
 
 register_css <<CSS
 
